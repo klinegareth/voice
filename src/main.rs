caaff3254a0d25085846ea3ab9ab5a42db140810
Kline Gareth <klinegareth@gmail.com>
2023-08-27 23:26:55 -0400

    Very messy, but she now says the right number of words

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use rand::Rng;
 use rodio::{source::Buffered, Decoder, OutputStream, Sink, Source};
 use std::convert::AsRef;
-use std::io;
 use std::io::BufReader;
 use std::path::Path;
+use std::{fs, io};
 use std::{fs::File, io::Write};
 
 // Count words in a string - very rough implementation
@@ -29,11 +30,33 @@ fn read_stdin() -> String {
     return buffer;
 }
 
+fn random_file_from_dir(dir: &str, range: i32) -> String {
+    let mut prefix: String = "voice".to_owned();
+    let mut rng = rand::thread_rng();
+    let index = rng.gen_range(0..range) + 1;
+    let suffix = ".wav";
+    let filename = format!("{dir}/{prefix}{index}{suffix}");
+    println!("random audio file: {}", filename);
+    return filename;
+}
+
+fn dir_file_count(dir: &str) -> i32 {
+    let mut count = 0;
+    println!("audio directory: {}", dir);
+    for file in fs::read_dir(dir) {
+        count += 1;
+    }
+    println!("{}", count);
+    return count;
+}
+
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
+    let audio_folder = "/Users/kline/projects/summer/georgeos/voice/src/audio";
     for _i in 0..count_words(&read_stdin()) {
-        let file = BufReader::new(File::open("voice1.wav").unwrap());
+        let rnd_file = random_file_from_dir(audio_folder, 6);
+        let file = BufReader::new(File::open(rnd_file).unwrap());
         let source = Decoder::new(file).unwrap();
         sink.append(source);
     }
