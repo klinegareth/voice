a4346c60022af1fdb744774cf6a95666426e6ad3
Kline Gareth <klinegareth@gmail.com>
2023-08-31 08:51:45 -0400

    First experiment in trying to get audio to line up with text

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,21 @@
 use rand::Rng;
+use rodio::{source, Sample};
 use rodio::{source::Buffered, Decoder, OutputStream, Sink, Source};
+use std::error::Error;
 use std::io::BufReader;
 use std::{fs, io};
 use std::{fs::File, io::Write};
 extern crate vader_sentiment;
 
-// Count words in a string - very rough implementation
-fn count_words(sentence: &str) -> i32 {
-    let mut result = 0;
-    let mut last_was_space = false;
-    for c in sentence.chars() {
-        if c.is_whitespace() {
-            if last_was_space == false {
-                result += 1;
-            }
-            last_was_space = true;
-        } else {
-            result += 0;
-            last_was_space = false;
-        }
-    }
-    return result;
+fn sentence_to_vec(sentence: &str) -> Vec<&str> {
+    let words = sentence.split_whitespace().collect();
+    return words;
 }
 
-fn read_stdin() -> String {
+fn read_stdin() -> Result<String, io::Error> {
     let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer);
-    return buffer;
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer)
 }
 
 fn random_file_from_dir(dir: &str, pre: &str) -> String {
@@ -46,29 +35,37 @@ fn dir_file_count(dir: &str) -> i32 {
     return count;
 }
 
-fn main() {
-    let sentence = read_stdin();
-    let analyzer = vader_sentiment::SentimentIntensityAnalyzer::new();
-    let emotion = analyzer.polarity_scores(&sentence);
+fn speak(sentence: &str, emotion: &f64) -> () {
+    let words = sentence_to_vec(sentence);
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
-    if emotion.get("compound").unwrap() >= &0.0 {
+    if emotion >= &0.0 {
         let audio_folder = "/Users/kline/projects/summer/georgeos/voice/src/audio/pos";
-        for _i in 0..count_words(&sentence) {
+        for _i in 0..words.len() {
             let rnd_file = random_file_from_dir(audio_folder, "voice");
             let file = BufReader::new(File::open(rnd_file).unwrap());
             let source = Decoder::new(file).unwrap();
-            sink.append(source);
+            sink.append(source)
         }
     } else {
         let audio_folder = "/Users/kline/projects/summer/georgeos/voice/src/audio/neg";
-        for _i in 0..count_words(&sentence) {
+        for _i in 0..words.len() {
             let rnd_file = random_file_from_dir(audio_folder, "neg");
             let file = BufReader::new(File::open(rnd_file).unwrap());
             let source = Decoder::new(file).unwrap();
             sink.append(source);
         }
     }
-    println!("{}", sentence);
-    sink.sleep_until_end();
+    for i in 0..words.len() {
+        println!("{}", words.get(i).unwrap().to_string());
+        std::thread::sleep(std::time::Duration::from_secs_f64(0.2));
+    }
+    std::thread::sleep(std::time::Duration::from_secs_f64(sink.len() as f64));
+}
+
+fn main() {
+    let sentence = read_stdin().unwrap();
+    let analyzer = vader_sentiment::SentimentIntensityAnalyzer::new();
+    let emotion = analyzer.polarity_scores(&sentence);
+    speak(&sentence, emotion.get("compound").unwrap());
 }
